47312ae6eaa7d86b679ea876e9a0b1c7649ab22e
Mauricio Velazquez <mauri_i9070@hotmail.com>
2020-10-22 19:05:20 -0300

    deploy

---
+++ src/components/ListCistas.vue
@@ -0,0 +1,26 @@
+<template>
+     <p>Mascota: <span>{{cita.mascota}}</span> </p>
+     <p>Dueño: <span>{{cita.propietario}}</span> </p>
+     <p>Fecha: <span>{{cita.fecha}}</span> </p>
+     <p>Hora: <span>{{cita.hora}}</span> </p>
+     <p>Sintomas: <span>{{cita.sintomas}}</span> </p>
+
+     <button @click="eliminarCita(cita.mascota)">Eliminar &times;</button>
+</template>
+
+<script>
+export default {
+     name: 'ListCitas',
+     props: {
+          cita: Object
+     },
+     methods: {
+          eliminarCita(mascota) {
+               this.$emit('eliminar-cita', this.citas.filter(cita => cita.mascota !== mascota))
+          }
+     }
+}
+</script>
+
+<style>
+</style>
